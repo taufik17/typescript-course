dd4874ba6282c8c665200e7e99d6ccd5f2e29bb5
taufik17 <taufikagungsantoso17@gmail.com>
2022-10-08 11:56:03 +0700

    array

---
+++ build/array.js
@@ -0,0 +1,14 @@
+"use strict";
+// Array
+let array;
+array = [1, 2, 3];
+let array2;
+array2 = [
+    'test1',
+    'test2'
+];
+let arraySembarang;
+arraySembarang = ['string', 2, true];
+// Tuples
+let biodata;
+biodata = ['taufik', 24, false];
---
+++ src/array.ts
@@ -0,0 +1,17 @@
+// Array
+let array: number[] 
+array = [1,2,3]
+
+let array2: string[]
+array2 = [
+    'test1',
+    'test2'
+]
+
+let arraySembarang: any[]
+arraySembarang = ['string', 2, true]
+
+// Tuples
+let biodata: [string, number, boolean]
+biodata = ['taufik', 24, false]
+
